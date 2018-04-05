3a1d2d76bffdcf0ef6f177264cb9ecc2855f5db3
HolyMichael <555migalves555@gmail.com>
2018-04-05 15:21:50 +0100

    poof rekt op

--- lojadebicicletas v2.0/src/lojadebicicletas/v2/pkg0/LojadebicicletasV20.java
+++ lojadebicicletas v2.0/src/lojadebicicletas/v2/pkg0/LojadebicicletasV20.java
@@ -15,7 +15,7 @@ public class LojadebicicletasV20 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("eu sou um meme");
     }
     
 }
