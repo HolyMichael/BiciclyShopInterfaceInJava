785ea5d3eb8e00c8fd3c43116861afc83b387aba
HolyMichael <555migalves555@gmail.com>
2018-04-14 15:41:29 +0100

    new servers up

---
+++ lojadebicicletas v2.0/src/lojadebicicletas/v2/pkg0/RMIInterface.java
@@ -0,0 +1,9 @@
+package lojadebicicletas.v2.pkg0;
+
+public interface RMIInterface extends java.rmi.Remote{
+    public java.util.Date getDate() throws java.rmi.RemoteException;
+    public int getCount() throws java.rmi.RemoteException;
+}
+
+
+
